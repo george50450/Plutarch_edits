65801cf0b2c25dafd7651af5681d234d79c562fc
george50450 <gtkalampokis@gmail.com>
2020-06-10 23:27:57 +0300

    Delete Old Project

--- Plutarch_Parallel_Lives/src/phaseAnalyzer/parser/IParser.java
+++
@@ -1,7 +0,0 @@
-package phaseAnalyzer.parser;
-
-import phaseAnalyzer.commons.TransitionHistory;
-
-public interface IParser {
-	public abstract TransitionHistory parse(String fileName, String delimeter);
-}
